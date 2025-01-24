9c02f37ff29175ba8b3e7118b2e811b81a6d1a9e
Team7034 <wlhsfrc@gmail.com>
2025-01-23 16:18:50 -0800

    Added two cameras - Sonan, Jack

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -2,41 +2,50 @@ package frc.robot.subsystems.swervedrive;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Vision {
 
 	AprilTagFieldLayout tagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
-	PhotonCamera centerCam = new PhotonCamera("center");
-	Transform3d centerCamToRobot = new Transform3d(new Translation3d(0.39, 0, 0), new Rotation3d(0,0,0));
-	PhotonPoseEstimator centerPoseEstimator = new PhotonPoseEstimator(tagFieldLayout, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, centerCamToRobot);
+	PhotonCamera frontCam, backCam;
+	Transform3d frontCamToRobot, backCamToRobot;
+	PhotonPoseEstimator frontPoseEstimator, backPoseEstimator;
 
 	/**
 	 *
 	 * @param
 	 */
 	public Vision() {
+		frontCam = new PhotonCamera("front");
+		backCam = new PhotonCamera("back");
+		frontCamToRobot = new Transform3d(new Translation3d(0.39, 0, 0), new Rotation3d(0,0,0));
+		backCamToRobot = new Transform3d(new Translation3d(-0.39, 0.0, 0), new Rotation3d(0,0, Math.PI));
+		frontPoseEstimator = new PhotonPoseEstimator(tagFieldLayout, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, frontCamToRobot);
+		backPoseEstimator = new PhotonPoseEstimator(tagFieldLayout, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, backCamToRobot);
 	}
 
 	public ArrayList<MeasurementWithTimeStamp> updateVisionOdometry() {
-		List<PhotonPipelineResult> centerPipelineResult = centerCam.getAllUnreadResults();
 		ArrayList<MeasurementWithTimeStamp> measurementWithTimeStamps = new ArrayList<>();
-		for(PhotonPipelineResult result : centerPipelineResult) {
-			var estimateOptional = centerPoseEstimator.update(result);
+		for(PhotonPipelineResult result : frontCam.getAllUnreadResults()) {
+			var estimateOptional = frontPoseEstimator.update(result);
+			estimateOptional.ifPresent(estimatedRobotPose ->
+					measurementWithTimeStamps.add(
+							new MeasurementWithTimeStamp(result.getTimestampSeconds(), estimatedRobotPose)));
+
+		}
+
+		for(PhotonPipelineResult result : backCam.getAllUnreadResults()) {
+			var estimateOptional = backPoseEstimator.update(result);
 			estimateOptional.ifPresent(estimatedRobotPose ->
 					measurementWithTimeStamps.add(
 							new MeasurementWithTimeStamp(result.getTimestampSeconds(), estimatedRobotPose)));
@@ -44,9 +53,6 @@ public class Vision {
 		}
 
 		return measurementWithTimeStamps;
-
-
-
 	}
 
 }
