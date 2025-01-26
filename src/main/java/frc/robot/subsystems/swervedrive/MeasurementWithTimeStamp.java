ab3b93edc8bb05c98bf34f9d60a1c9754a77d67d
Team7034 <wlhsfrc@gmail.com>
2025-01-25 16:55:11 -0800

    Added some paths as well as a configurable OTF path.

--- src/main/java/frc/robot/subsystems/swervedrive/MeasurementWithTimeStamp.java
+++ src/main/java/frc/robot/subsystems/swervedrive/MeasurementWithTimeStamp.java
@@ -1,14 +1,19 @@
 package frc.robot.subsystems.swervedrive;
 
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import org.photonvision.EstimatedRobotPose;
 
 public class MeasurementWithTimeStamp {
 	private double timeStamp;
 	private EstimatedRobotPose robotPose;
-	public MeasurementWithTimeStamp(double timeStamp, EstimatedRobotPose robotPose) {
+	private Matrix<N3, N1> stdDevs;
+	public MeasurementWithTimeStamp(double timeStamp, EstimatedRobotPose robotPose, Matrix<N3, N1> stdDevs) {
 		this.timeStamp = timeStamp;
 		this.robotPose = robotPose;
+		this.stdDevs = stdDevs;
 	}
 
 	public double getTimeStamp() {
@@ -17,5 +22,9 @@ public class MeasurementWithTimeStamp {
 
 	public Pose2d getRobotPose() {
 		return robotPose.estimatedPose.toPose2d();
+	}
+
+	public Matrix<N3, N1> getStdDevs() {
+		return stdDevs;
 	}
 }
