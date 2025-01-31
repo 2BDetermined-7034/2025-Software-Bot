f595d66437bb3944d63aa04ed3f3200dc4d76823
Beatrice Gilfix <gilfixbea@gmail.com>
2025-01-30 21:02:47 -0800

    Created a Reef object (subsystem) intended to act as a singleton, defined central logic wrote constructor and paired methods *periodic method is not fully written *logic is not fully implemented

---
+++ src/main/java/frc/robot/subsystems/qrfc/Reef.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems.qrfc;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.Arrays;
+
+public class Reef {
+
+    /*
+    STEPS:
+    1. Know what is new (in the arrays) - DONE
+    2. Get rid of old reference of reef status - DONE
+    3. Replace w/ new positional copy - DONE
+    4. Send back corrected reef status - IN PROGRESS
+    5. Store correct pose (be prepared to relay this in the future) - NOT STARTED
+     */
+    //Each indice in m_reefStatus correlates to its sister pose in the pose array
+    private final Pose2d[] reefPoses = new Pose2d[12];
+
+    private boolean[] onboardReefStatus = new boolean[12];
+
+    private int dodecagonLookat;
+
+    public Reef () {
+
+    }
+
+
+    private Pose2d getReefDestinationPose(int i){
+        return reefPoses[i];
+    };
+
+    /**
+     * Compares local copy and new user input data (a similar boolean array of the same length)
+     * and hunts down and returns the <STRONG>indice of where the reef should be green at</STRONG>
+     * @param old Cached boolean array of the last known states of the reef GUI
+     * @return -1 if userInput has not selected location. Else returns value [0,11].
+     */
+    public int findNewInputIndice(boolean[] old) {
+        boolean[] userInput = SmartDashboard.getBooleanArray("Reef", onboardReefStatus);
+        for(int i = 0 ; i <= 11; ++i) {
+            if(userInput[i] && !old[i] ) { //true vs true overlap
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    /**
+     * Overwrites onboardReefStatus with an all false status, then replaces the newReefIndex parameter index with true
+     * @param newReefIndex index in the array that's going to be true :3c
+     */
+    public void changeReefPosition(int newReefIndex) {
+        Arrays.fill(onboardReefStatus,false); //Clear onboard reef status
+        if(newReefIndex != -1){
+            onboardReefStatus[newReefIndex] = true;
+        }
+    }
+
+    /**
+     * Called periodically in singleton instance
+     */
+    public void process() {
+        int indice = findNewInputIndice(onboardReefStatus);
+        changeReefPosition(indice);
+        SmartDashboard.putBooleanArray("Reef", onboardReefStatus);
+    }
+}
