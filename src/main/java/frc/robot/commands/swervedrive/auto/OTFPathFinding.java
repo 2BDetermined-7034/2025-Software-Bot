cea84b8ca55ccc9c58c0491132eaae08f4fefca4
Team7034 <wlhsfrc@gmail.com>
2025-02-08 13:51:40 -0800

    Added all reef positions and method for getting there - Alex

--- src/main/java/frc/robot/commands/swervedrive/auto/OTFPathFinding.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/OTFPathFinding.java
@@ -2,30 +2,58 @@ package frc.robot.commands.swervedrive.auto;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class OTFPathFinding {
 
-    public static void main(String[] args)
+        //edit to the positions that we want the robot to go to
+        //They are currently the positions of the april tags
+
+        private Pose2d[] bluePoseArray = new Pose2d[]
+        {
+                new Pose2d(3.920,3.305,new Rotation2d(240)),
+                new Pose2d(3.458,4.967,new Rotation2d(180)),
+                new Pose2d(4.920,4.750,new Rotation2d(120)),
+                new Pose2d(4.902,4.950,new Rotation2d(60)),
+                new Pose2d(5.535,4.233,new Rotation2d(0)),
+                new Pose2d(5.102,3.305,new Rotation2d(300)),
+
+                new Pose2d(4.080,3.105,new Rotation2d(240)),
+                new Pose2d(3.258,4.233,new Rotation2d(180)),
+                new Pose2d(4.080,4.950,new Rotation2d(120)),
+                new Pose2d(5.102,4.750,new Rotation2d(60)),
+                new Pose2d(5.535,4.967,new Rotation2d(0)),
+                new Pose2d(4.902,3.105,new Rotation2d(300))
+        };
+
+        private Pose2d[] redPoseArray = new Pose2d[]
+        {
+                new Pose2d(13.693,3.305,new Rotation2d(240)),
+                new Pose2d(14.102,4.233,new Rotation2d(180)),
+                new Pose2d(13.493,4.950,new Rotation2d(120)),
+                new Pose2d(12.450,4.950,new Rotation2d(60)),
+                new Pose2d(12.033,4.967,new Rotation2d(0)),
+                new Pose2d(12.650,3.105,new Rotation2d(300)),
+
+                new Pose2d(13.493,3.205,new Rotation2d(240)),
+                new Pose2d(14.102,3.967,new Rotation2d(180)),
+                new Pose2d(13.893,4.750,new Rotation2d(120)),
+                new Pose2d(12.650,4.950,new Rotation2d(60)),
+                new Pose2d(12.033,4.233,new Rotation2d(0)),
+                new Pose2d(12.450,3.305,new Rotation2d(300))
+        };
+
+    public Command goToPos(SwerveSubsystem swerve, int scoreLocation)
     {
-
-        Pose2d[] bluePoseArray = new Pose2d[]
-                {
-                        new Pose2d(4.080,3.305,new Rotation2d(0)),
-                        new Pose2d(3.658,4.033,new Rotation2d(0)),
-                        new Pose2d(4.080,4.750,new Rotation2d(0)),
-                        new Pose2d(4.902,4.750,new Rotation2d(0)),
-                        new Pose2d(5.335,4.033,new Rotation2d(0)),
-                        new Pose2d(4.902,3.305,new Rotation2d(0))
-                };
-
-        Pose2d[] redPoseArray = new Pose2d[]
-                {
-                        new Pose2d(13.493,3.305,new Rotation2d(0)),
-                        new Pose2d(13.902,4.033,new Rotation2d(0)),
-                        new Pose2d(13.493,4.750,new Rotation2d(0)),
-                        new Pose2d(12.650,4.750,new Rotation2d(0)),
-                        new Pose2d(12.233,4.033,new Rotation2d(0)),
-                        new Pose2d(12.650,3.305,new Rotation2d(0))
-                };
+        Alliance alliance = DriverStation.getAlliance().get();
+        if(alliance == Alliance.Blue)
+        {
+                return swerve.driveToPose(bluePoseArray[scoreLocation]);
+        }else{
+                return swerve.driveToPose(redPoseArray[scoreLocation]);
+        }
     }
 }
